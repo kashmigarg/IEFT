684d51bece1dde4b9b01affd734daa8272c2fd3f
an19352 <piyushbajaj71@gmail.com>
2022-12-04 04:21:47 +0000

    Update cw.py

--- cw.py
+++ cw.py
@@ -13,13 +13,13 @@ order_interval = 60
 order_sched = {'sup': supply_schedule, 'dem': demand_schedule,
                'interval': order_interval, 'timemode': 'drip-poisson'}
 
-sellers_spec = [('ZIC', 19), ('PRSH', 30, {'k': 8, 's_min': -1.0, 's_max': 1.0})]
-buyers_spec = [('ZIC', 20)]
+sellers_spec = [('ZIC', 29), ('PRDE', 1, {'k': 4, 's_min': -1.0, 's_max': 1.0})]
+buyers_spec = [('ZIC', 30)]
 traders_spec = {'sellers':sellers_spec, 'buyers':buyers_spec}
 
 trial = 1
-trial_id = '1PRDEK8_MZIC' + str(trial)
-tdump = open('1PRDEK8_MZIC' + str(trial) + '_avg_balance.csv', 'w')
+trial_id = '1PRDEK4_MZIC' + str(trial)
+tdump = open('1PRDEK4_MZIC' + str(trial) + '_avg_balance.csv', 'w')
 dump_all = True
 verbose = False
 market_session(trial_id, start_time, end_time, traders_spec, order_sched, tdump, dump_all, verbose)
